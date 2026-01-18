38fbc6ba84e65290cafe302cf81829d7c90bd211
EaseDo <111581553+DanielSun024@users.noreply.github.com>
2026-01-18 20:33:33 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -604,6 +604,19 @@ export async function onRequest(context) {
 
     
 
+    const horizontalAllLink = `
+
+        <div class="menu-item-wrapper relative inline-block text-left">
+
+          <a href="?catalog=all" class="nav-btn ${allLinkClass} ${allLinkActiveMarker}">
+
+              全部
+
+          </a>
+
+        </div>
+
+    `;
 
     
 
